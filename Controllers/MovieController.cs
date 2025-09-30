1c8dbacb096cfac71f86f56cf932a218d57c22e9
Dante Wong <dkwong@coastal.edu>
2025-09-30 01:34:49 -0400

    Initial commit: working MovieApi Project

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieApi.Models;
+using MovieApi.Services;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+
+        private readonly ILogger<MovieController> _logger;
+        private IMovieService _service;
+
+        public MovieController(ILogger<MovieController> logger, IMovieService service)
+        {
+            _logger = logger;
+            _service=service;
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            IEnumerable<Movie> list = _service.GetMovies();
+            if(list != null)
+                return Ok(list);
+            else
+                return BadRequest();
+
+        }
+
+        [HttpGet("{name}", Name="GetMovie")]
+        public IActionResult GetMovieByName(string name)
+        {
+            Movie obj = _service.GetMovieByName(name);
+            if(obj!=null)
+                return Ok(obj);
+        
+            return BadRequest();
+
+        }
+
+        [HttpGet("year/")]
+        public IActionResult GetMovieByYear(int year)
+        {
+            Movie obj = _service.GetMovieByYear(year);
+            if(obj!=null)
+                return Ok(obj);
+            return BadRequest();
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateMovie(Movie m)
+        {
+            _service.CreateMovie(m);
+            //add some code to determine if successful
+            return CreatedAtRoute("GetMovie", new {name=m.Name}, m);
+
+        }
+
+        [HttpPut("{name}")]
+        public IActionResult UpdateMovie(string name, Movie movieIn)
+        {
+            _service.UpdateMovie(name, movieIn);
+
+            return NoContent();
+            
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult DeleteMovie(string name)
+        {
+            _service.DeleteMovie(name);
+            return NoContent();
+
+
+        }
+    }
+}
---
+++ Services/IMovieServices.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using MovieApi.Models;
+
+namespace MovieApi.Services
+{
+    public interface IMovieService
+    {
+        public IEnumerable<Movie> GetMovies();
+
+        public Movie GetMovieByName(string name);
+
+        public Movie GetMovieByYear(int year); //Or public Movie GetMovieByYear
+
+        public void CreateMovie(Movie m);
+
+        public void UpdateMovie(string name, Movie m);
+
+        public void DeleteMovie(string name);
+    }
+}
